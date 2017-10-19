32bba1823829c63033d8cbe2f58d9e6972e5aa6e
liuyi <liuyi@ushow.media>
2017-10-19 13:39:42 +0800

    呃

--- src/main/java/com/doopp/gauss/server/configuration/EhcacheConfiguration.java
+++ src/main/java/com/doopp/gauss/server/configuration/EhcacheConfiguration.java
@@ -1,26 +1,28 @@
-//package com.doopp.gauss.server.configuration;
-//
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.cache.ehcache.EhCacheCacheManager;
-//import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.io.ClassPathResource;
-//
-//@Configuration
-//public class EhcacheConfiguration {
-//
-//    @Bean
-//    public EhCacheManagerFactoryBean ehCacheManagerFactoryBean() {
-//        EhCacheManagerFactoryBean ehCacheManagerFactoryBean = new EhCacheManagerFactoryBean();
-//        ehCacheManagerFactoryBean.setConfigLocation(new ClassPathResource("config/ehcache/ehcache.xml"));
-//        return ehCacheManagerFactoryBean;
-//    }
-//
-//    @Bean
-//    public EhCacheCacheManager ehCacheCacheManager (@Qualifier("ehCacheManagerFactoryBean") EhCacheManagerFactoryBean ehCacheManagerFactoryBean) {
-//        EhCacheCacheManager ehCacheCacheManager = new EhCacheCacheManager();
-//        ehCacheCacheManager.setCacheManager(ehCacheManagerFactoryBean.getObject());
-//        return ehCacheCacheManager;
-//    }
-//}
+package com.doopp.gauss.server.configuration;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+@EnableCaching
+public class EhcacheConfiguration {
+
+    @Bean
+    public EhCacheManagerFactoryBean ehCacheManagerFactoryBean() {
+        EhCacheManagerFactoryBean ehCacheManagerFactoryBean = new EhCacheManagerFactoryBean();
+        ehCacheManagerFactoryBean.setConfigLocation(new ClassPathResource("config/ehcache/ehcache.xml"));
+        return ehCacheManagerFactoryBean;
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        EhCacheCacheManager cacheManager = new EhCacheCacheManager();
+        cacheManager.setCacheManager(ehCacheManagerFactoryBean().getObject());
+        return cacheManager;
+    }
+}
