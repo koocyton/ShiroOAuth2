0e496f0be514b60355531332b5a71a3759b72371
koocyton <koocyton@gmail.com>
2017-10-21 11:46:02 +0800

    调整页面，和优化 js 结构

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -1,14 +1,16 @@
+let listController = function($scope) {
+    $scope.lists = [
+        {"id": "1", "name": "张三", "age": 22, "phoneNum": "18298705786"},
+        {"id": "2", "name": "王五", "age": 28, "phoneNum": "18456705786"},
+        {"id": "3", "name": "张三", "age": 29, "phoneNum": "18291235786"},
+        {"id": "4", "name": "蔡雄", "age": 22, "phoneNum": "18298705786"},
+        {"id": "5", "name": "张三", "age": 26, "phoneNum": "18759705786"},
+        {"id": "6", "name": "张楚", "age": 22, "phoneNum": "18298705786"}
+    ]
+};
+
 angular.module('ngRouteChatApp', ['ngRoute'])
-    .controller("listCtl", function($scope) {
-        $scope.lists = [
-            {"id": "1", "name": "张三", "age": 22, "phoneNum": "18298705786"},
-            {"id": "2", "name": "王五", "age": 28, "phoneNum": "18456705786"},
-            {"id": "3", "name": "张三", "age": 29, "phoneNum": "18291235786"},
-            {"id": "4", "name": "蔡雄", "age": 22, "phoneNum": "18298705786"},
-            {"id": "5", "name": "张三", "age": 26, "phoneNum": "18759705786"},
-            {"id": "6", "name": "张楚", "age": 22, "phoneNum": "18298705786"}
-        ]
-    })
+    .controller("listCtl", listController)
     .config([
         '$routeProvider',
         '$locationProvider',
